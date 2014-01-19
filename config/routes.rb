8bd31e4fa1eadf27e451a70a1eee30854556b7a7
j3ck <fox_raiodin@mail.ru>
2014-01-19 16:46:09 +0300

    Added about oage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 GuestBook::Application.routes.draw do
 
   resources :reviews
+  get '/about' => 'static_pages#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
