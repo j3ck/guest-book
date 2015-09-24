ba970a59a3f97f485a16ecf9aced691c4b44dd68
Eugene Stanko <j3ck.dev@gmail.com>
2015-09-24 19:46:56 +0300

    added simple api

--- app/grape/api/mobile/root.rb
+++ app/grape/api/mobile/root.rb
@@ -5,9 +5,15 @@ module Api::Mobile
     format :json
     prefix :api
 
+    rescue_from Grape::Exceptions::ValidationErrors do |e|
+      rack_response({status: 'error', error: e.message}.to_json, 400)
+    end
+
     get :ping do
       {status: 'ok'}
     end
+
+    mount Api::Mobile::Reviews
 
     add_swagger_documentation hide_documentation_path: true
   end
